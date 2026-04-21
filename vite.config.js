1e3e824177da9294fd4311548cae854bbb2ce558
unknown <daneshwarsingh2404@gmail.com>
2026-04-21 13:04:17 +0530

    catalyst inbuilt auth4

--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,15 @@
 // });
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'   // ✅ REQUIRED for Catalyst
-  }
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist'   // ✅ REQUIRED for Catalyst
+//   }
+// })
 
 
 import { defineConfig } from 'vite'
