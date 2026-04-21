1206f243ad7d04ac52528b64c4cc078d88ea07ab
unknown <daneshwarsingh2404@gmail.com>
2026-04-21 12:52:49 +0530

    catalyst inbuilt auth2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "../backend new/client",
+//     emptyOutDir: false
+//   }
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../backend new/client",
-    emptyOutDir: false
+    outDir: 'dist'   // ✅ REQUIRED for Catalyst
   }
-});
+})
